503dbef6c43b26af55d53dd49e1c58f53ad9458f
LOVELYZOMBIEYHO <yimwingho2003@yahoo.com.hk>
2022-07-18 14:43:13 -0400

    connect vps6 https commit

--- pages/videos/[slug].js
+++ pages/videos/[slug].js
@@ -222,10 +222,11 @@ export default function PostPage({ posts, navbarOptions }) {
 // This function gets called at build time on server-side.
 // It may be called again, on a serverless function, if
 // revalidation is enabled and a new request comes in
+// -------------------
 export async function getStaticProps({ params: { slug } }) {
   const res = await fetch(`${API_URL}/post/${slug}`);
   const posts = await res.json();
-
+  console.log(slug);
   // Dynamic category colorKey and options
   const resCategoryOptions = await fetch(`${API_URL}/categoryoptions`);
   const categoryOptions = await resCategoryOptions.json();
@@ -275,5 +276,63 @@ export async function getStaticPaths() {
   // We'll pre-render only these paths at build time.
   // { fallback: blocking } will server-render pages
   // on-demand if the path doesn't exist.
-  return { paths, fallback: true };
+  return { paths, fallback: "blocking" };
 }
+// -------------------------
+
+// export async function getStaticProps({ params: { slug } }) {
+//   console.log(slug);
+//   const res = await fetch(`${API_URL}/post/idcount/${slug}`);
+//   const posts = await res.json();
+
+//   // Dynamic category colorKey and options
+//   const resCategoryOptions = await fetch(`${API_URL}/categoryoptions`);
+//   const categoryOptions = await resCategoryOptions.json();
+
+//   // Dynamic navbar options
+//   const resNavbarOptions = await fetch(`${API_URL}/navbardynamic`);
+//   const navbarOptions = await resNavbarOptions.json();
+
+//   // // Dynamic add pageview to MongoDB
+//   // const updateDBViewCount = await posts[0].idCount;
+
+//   // const resUpdateDBViewCount = await fetch(
+//   //   `https://api.countapi.xyz/get/jseedav.com/:PATHNAME:${updateDBViewCount}`
+//   // );
+//   // const resUpdateDBViewCountJson = await resUpdateDBViewCount.json();
+//   // const requestForUpdateDBViewCount = await fetch(`${API_URL}/pageviewcount`);
+//   // const gi = await console.log(resUpdateDBViewCountJson.value);
+
+//   return {
+//     props: {
+//       posts,
+//       categoryOptions,
+//       navbarOptions,
+//     },
+//     // Next.js will attempt to re-generate the page:
+//     // - When a request comes in
+//     // - At most once every 10 seconds
+//     revalidate: parseInt(revalidateTimeVariable), // In seconds
+//   };
+// }
+// // 求其PICK 一個POST 作為首次RENDER 的POST， 若可行，可npm run build & start （可在FLASK 後台測試有沒有每次更新F5 都會FETCH DATA
+
+// // // This function gets called at build time on server-side.
+// // // It may be called again, on a serverless function, if
+// // // the path has not been generated.
+// export async function getStaticPaths() {
+//   // this URL is created specifically for fetch first time of ISR
+//   const res = await fetch(`${API_URL}/postforisrfirst`);
+//   // const res = await fetch(`${API_URL}/post`);
+//   const posts = await res.json();
+
+//   // Get the paths we want to pre-render based on posts
+//   const paths = posts.map((post) => ({
+//     params: { slug: post.idCount },
+//   }));
+
+//   // We'll pre-render only these paths at build time.
+//   // { fallback: blocking } will server-render pages
+//   // on-demand if the path doesn't exist.
+//   return { paths, fallback: "blocking" };
+// }
